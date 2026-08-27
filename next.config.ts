5d494ef9fca7259fed553335581b4ab17c1be4e6
Muhammad Hamza <136830850+hamza2130@users.noreply.github.com>
2026-08-27 14:22:15 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
-  reactStrictMode: true
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL:
+      process.env.NEXT_PUBLIC_API_URL ||
+      "https://kindervale-backend.onrender.com/api",
+  },
 };
 
 export default nextConfig;
